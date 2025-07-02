834657c56660845c5041cbcd37127ca6287ff4a2
jakmate <jakub.orzolek04@gmail.com>
2025-07-02 21:12:32 +0100

    fix: render.yaml

--- backend/main.py
+++ backend/main.py
@@ -795,5 +795,6 @@ def convert_race_timezone(race, target_timezone):
         return race
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT, log_level="info", reload=False)
+# if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 8000))
+#    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info", reload=False)
